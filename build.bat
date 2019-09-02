04f53cde88f2e7e666003cdc540f5664577c7d15
Azure DevOps <devops@microsoft.com>
2019-09-02 06:19:01 +0200

    start of azdo build agent customisation

--- build.bat
+++ build.bat
@@ -4,9 +4,18 @@
 
 ::docker build -t f2calvdev:latest -f ubuntu16.04-and-dotnetcore3.0-preview8/Dockerfile .
 ::docker build -t f2calvdev:latest -f ubuntu18.04-and-dotnetcore3.0-preview8/Dockerfile .
-docker build -t f2calvdev:latest -f ubuntu19.04-and-dotnetcore3.0-preview8/Dockerfile .
+::docker build -t f2calvdev:latest -f ubuntu19.04-and-dotnetcore3.0-preview8/Dockerfile .
+
+
+::cd azdo-linux-agent
+::docker build -t f2calvdev:latest .
+
+::cd azdo-linux-agent-dotnetcore3.0-preview8
+::docker build -t f2calvdev:latest .
+
 
 docker run --name f2calvdev --rm -it f2calvdev:latest
+::docker image rm f2calvdev:latest
 
 ::docker exec -it
 
